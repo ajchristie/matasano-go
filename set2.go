45aeef0aab389bf0d8a6ea36afbe491b998105e0
Aaron Christie <poor.yorick@hotmail.com>
2018-12-20 16:34:42 -0700

    Update

--- set2.go
+++ set2.go
@@ -2,11 +2,42 @@ package set2
 
 import (
   "fmt"
-  "set1"
-  "crypto/rand"
-  rd "math/rand"
+  "encoding/base64"
+  "github.com/ajchristie/set1"
+  crand "crypto/rand"
+  "math/rand"
+  "encoding/binary"
 )
 
+// set up some easier access to cryptographically secure random integers
+// usage: declare var src cSource then use rand.New with this source
+// methods are provided for both Source and Source64 types
+type cSource struct {}
+
+func (s cSource) Seed(seed int64) {} // no need to seed; we'll pull random from dev
+
+func (s cSource) Uint64() uint64 {
+  var v uint64
+  err := binary.Read(crand.Reader, binary.BigEndian, &v)
+  if err != nil {
+    log.Fatal(err)
+  }
+  return v
+}
+
+func (s cSource) Int63() int64 {
+  return int64(s.Uint64() &^ uint64(1<<63))
+}
+
+// a function for secure random bytes
+func RandomBytes(n int) ([]byte, error) {
+  bytes := make([]byte, n)
+  _, err := crand.Read(b)
+  if err != nil { // error is simply returned and should be checked after call
+    return nil, err
+  }
+  return b, err
+}
 
 // for challenge 9
 func PKCS7(b []byte, n int) []byte {
@@ -45,7 +76,7 @@ func EncAES128ECB(intext, key []byte) []byte {
   var numBlocks int = len(intext) / aes.BlockSize
   outtext := make([]byte, len(text))
   for i := 0; i < numBlocks; i++ {
-    outtext[aes.BlockSize*i:aes.BlockSize*(i+1)] = op(intext[aes.BlockSize*i:])
+    outtext[aes.BlockSize*i:aes.BlockSize*(i+1)] = cipher.Encrypt(intext[aes.BlockSize*i:])
   }
   return outtext
 }
@@ -53,22 +84,24 @@ func EncAES128ECB(intext, key []byte) []byte {
 // for challenge 11
 func EncryptionOracle(ptext []byte) []byte {
   key := make([]byte, 16)
-  _, err := rand.Read(key)
+  _, err := crand.Read(key)
   if err != nil {
     panic("Error:", err)
   }
-  f := rd.Intn(5) + 5
-  b := rd.Intn(5) + 5
+  var src cSource
+  rnd = rand.New(src)
+  f := rnd.Intn(6) + 5 // these are always going to be the same w/o seeding?
+  b := rnd.Intn(6) + 5
   n : = f + len(ptext) + b
   messed := make([]byte, n)
   for i := 0; i < f; i++ {
-    messed[i] = rd.Intn(255)
+    messed[i] = rnd.Intn(255)
   }
   for i := f + len(ptext); i < n; i++ {
-    messed[i] = rd.Intn(255)
+    messed[i] = rnd.Intn(255)
   }
   copy(messed[f:f+len(ptext)], ptext)
-  choice := rd.Intn(1)
+  choice := rnd.Intn(2)
   if choice == 0 {
     return EncAES128ECB(messed, key)
   } else {
@@ -90,3 +123,49 @@ func OracleDetector() string {
 }
 
 // for challange 12
+func ECBOracle(ptext []byte) []byte {
+  key, err := RandomBytes(16)
+  if err != nil {
+    panic(err)
+  }
+  Tail := base64.DecodeString("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3AIE5vLCBJIGp1c3QgZHJvdmUgYnkK")
+  ptext = append(ptext, Tail...)
+  return EncAES128ECB(ptext, key)
+}
+
+func FindSizes() (blockSize, targetSize int) { // normally you'd pass in the encryption oracles
+  startLength := len(ECBOracle([]byte("")))
+  ptext := make([]byte, 0)
+  ctext := make([]byte, 0)
+  for {
+    ptext = append(ptext, 255)
+    ctext = ECBOracle(ptext)
+    if len(ctext) != startLength {
+      return len(ctext) - startLength, startLength - len(ptext)
+    }
+  }
+}
+
+func ByteXByteDecrypt() string {
+  blockSize := 16
+  targetSize := 138
+  padLength := (blockSize - (targetSize % blockSize)) % blockSize
+  attackIndex := targetSize + padLength - 1 // position for decryption
+  leader := make([]byte, targetSize + padLength - 1)
+  for i := range leader {
+    leader[i] = 255
+  }
+  targetDecryption := make([]byte, targetSize)
+  for j := 1; j <= targetSize; j++ {
+    targetBlock := ECBOracle(leader)[attackIndex-blockSize:attackIndex+1]
+    for i := 0; i <= 256; i++ {
+      scanBlock := ECBOracle(leader + targetProgress + i)[attackIndex-blockSize:attackSize+1]
+      if scanBlock == targetBlock {
+        targetDecryption[j-1] = i
+        break
+      }
+    }
+    leader = leader[:len(leader)-j]
+  }
+  return string(targetDecryption)
+}
